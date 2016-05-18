56b9cc806f9940cf0f1b1351ea54158c1062b269
Zhorander <elias_50@live.com>
2016-05-18 14:43:30 -0600

    added selection sort to header file

--- sort.h
+++ sort.h
@@ -12,6 +12,7 @@ void bubble_sort (void *list, int (*compar)(void *arg1, void *arg2), size_t size
 void merge_sort  (void *list, int (*compar)(void *arg1, void *arg2), size_t size, size_t base, size_t n_mem);
 void merge       (void *list, int (*compar)(void *arg1, void *arg2), size_t size, size_t base, size_t mid, size_t last);
 void bogo_sort   (void *list, int (*compar)(void *arg1, void *arg2), size_t size, size_t n_mem);
+void select_sort (void *list, int (*compar)(void *arg1, void *arg2), size_t size, size_t n_mem);
 int sorted       (void *list, int (*compar)(void *arg1, void *arg2), size_t size, size_t n_mem);
 void count_sort  (int *list, size_t n_mem);
 int max_of_list  (int *list, size_t n_mem);
